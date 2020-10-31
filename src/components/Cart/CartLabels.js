6cb22c5f64598752fcba705bb0542619dafe9504
RaufAl-hassan <alhassanr017@gmail.com>
2020-10-31 05:40:10 +0000

    fixed cart label appearing on small screens

--- src/components/Cart/CartLabels.js
+++ src/components/Cart/CartLabels.js
@@ -7,22 +7,22 @@ export default function CartLabels() {
         Your <span className="text-main">Cart</span>
       </h2>
       <div className="row">
-        <div className="col-2 d-sm-none d-lg-block text-center">
+        <div className="col-2 d-none d-lg-block text-center">
           <h6>Products</h6>
         </div>
-        <div className="col-2 d-sm-none d-lg-block text-center">
+        <div className="col-2 d-none d-lg-block text-center">
           <h6>Name Of Product</h6>
         </div>
-        <div className="col-2 d-sm-none d-lg-block text-center">
+        <div className="col-2 d-none d-lg-block text-center">
           <h6>Price</h6>
         </div>
-        <div className="col-2 d-sm-none d-lg-block text-center">
+        <div className="col-2 d-none d-lg-block text-center">
           <h6>Quatity</h6>
         </div>
-        <div className="col-2 d-sm-none d-lg-block text-center">
+        <div className="col-2 d-none d-lg-block text-center">
           <h6>Remove</h6>
         </div>
-        <div className="col-2 d-sm-none d-lg-block text-center">
+        <div className="col-2 d-none d-lg-block text-center">
           <h6>Total</h6>
         </div>
       </div>
